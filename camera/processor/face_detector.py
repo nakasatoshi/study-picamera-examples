f684c53048e45303679bbeda4487b537f4001540
nakasatoshi <38076018+nakasatoshi@users.noreply.github.com>
2018-05-31 21:06:26 +0900

    Update face_detector.py

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -29,12 +29,16 @@ class FaceDetector(object):
 
     def process_image(self, frame):
         # opencvでframe(カラー画像)をグレースケールに変換
-
+        img = cv2.imread(frame)
+        
         # 上記でグレースケールに変換したものをインスタンス化した顔分類器の
         # detectMultiScaleメソッドで処理し、認識した顔の座標情報を取得する
+        faces = face_cascade.detectMultiScale(img, 1.3, 5)
         
         # 取得した座標情報を元に、cv2.rectangleを使ってframe上に
         # 顔の位置を描画する
-
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+        
         # frameを戻り値として返す
         return frame
